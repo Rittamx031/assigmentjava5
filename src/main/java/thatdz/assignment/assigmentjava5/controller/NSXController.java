361c8be5c130adc2345f85a1b1b3506d9e58a510
Rittam <thatv12122003@gmail.com>
2023-06-04 00:25:28 +0700

    NSX service + repo OK

---
+++ src/main/java/thatdz/assignment/assigmentjava5/controller/NSXController.java
@@ -0,0 +1,24 @@
+package thatdz.assignment.assigmentjava5.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import thatdz.assignment.assigmentjava5.entity.NSX;
+import thatdz.assignment.assigmentjava5.service.NSXService;
+
+@Controller
+@RequestMapping("nsx")
+public class NSXController {
+    @Autowired
+    public NSXService service;
+    @GetMapping("index")
+    public String getMethodName() {
+        for (NSX entity : service.getNSXs()) {
+            System.out.println(entity.toString());
+        }
+        return "home/index.html";
+    }
+    
+}
--- src/main/java/thatdz/assignment/assigmentjava5/service/NSXService.java
+++ src/main/java/thatdz/assignment/assigmentjava5/service/NSXService.java
@@ -1,5 +1,43 @@
 package thatdz.assignment.assigmentjava5.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import thatdz.assignment.assigmentjava5.entity.NSX;
+import thatdz.assignment.assigmentjava5.repository.NSXIRepo;
+@Service
 public class NSXService {
+    @Autowired
+    public NSXIRepo repository;
+    public NSX saveNSX(NSX nsx) {
+        return repository.save(nsx);
+    }
+    public List<NSX> saveNSXs(List<NSX> nsxs) {
+        return repository.saveAll(nsxs);
+    }
+
+    public List<NSX> getNSXs() {
+        return repository.findAll();
+    }
+
+    public NSX getNSXById(UUID id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public String deleteNSX(UUID id) {
+        repository.deleteById(id);
+        return "NSX removed !! " + id;
+    }
+
     
+    public NSX updateNSX(NSX nsx) {
+        NSX existingNSX = repository.findById(nsx.getId()).orElse(null);
+        existingNSX.setId(nsx.getId());
+        existingNSX.setMa(nsx.getMa());
+        existingNSX.setTen(nsx.getTen());
+        return repository.save(existingNSX);
+    }
 }
