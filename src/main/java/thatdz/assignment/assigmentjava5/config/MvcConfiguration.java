99d5b5e531e95c337a83e9a886e149ba49cdb362
Rittam <thatv12122003@gmail.com>
2023-10-05 17:03:26 +0700

    update layout

---
+++ src/main/java/thatdz/assignment/assigmentjava5/config/MvcConfiguration.java
@@ -0,0 +1,23 @@
+package thatdz.assignment.assigmentjava5.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+  @Bean
+  public LayoutDialect layoutDialect() {
+    return new LayoutDialect();
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+    viewControllerRegistry.addViewController("/admin").setViewName("redirect:manager/home");
+    viewControllerRegistry.addViewController("/").setViewName("redirect:thatpee/index");
+  }
+
+}
